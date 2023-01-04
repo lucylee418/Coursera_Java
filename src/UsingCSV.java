c34a578bf9a15c78d0169a7c7d358234f8afad49
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-04 11:06:19 -0500

    git status

--- src/UsingCSV.java
+++ src/UsingCSV.java
@@ -3,20 +3,27 @@ import org.apache.commons.csv.*;
 
 public class UsingCSV {
 
-    public void readFood(){
-        FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
-        for(CSVRecord record : parser) {
-            System.out.println(record.get("Name"));
-        }
-    }
+    // public void readFood(){
+    //     FileResource fr = new FileResource();
+    //     CSVParser parser = fr.getCSVParser();
+    //     for(CSVRecord record : parser) {
+    //         System.out.println(record.get("Name"));
+    //     }
+    // }
 
     public void listExporters(CSVParser parser, String exportOfInterest) {
         for (CSVRecord record : parser) {
             String export = record.get("Exports");
             if (export.contains(exportOfInterest)) {
-                
+                String country = record.get("Country");
+                System.out.println(country);
             }
         }
+    }
+
+    public void whoExportsCoffee() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        listExporters(parser, "coffee");
     }
 }
