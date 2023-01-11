0fb14838973e9a543d1cbb01a97cc8088ac296ce
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-11 17:17:52 -0500

    Created

---
+++ src/CaesarCipher.java
@@ -0,0 +1,12 @@
+import edu.duke.*;
+
+public class CaesarCipher {
+
+    public String encrypt(String input, int key) {
+
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+
+}
