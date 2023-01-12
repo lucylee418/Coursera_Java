027a9db577b0cb46d7b0d5b6c9198bc2b2c6088e
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-12 12:18:48 -0500

    Updated

--- src/WordLengths.java
+++ src/WordLengths.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class WordLengths {
 
-    public void countWordLengths(FileResource resource, int[] counts){
+    public int[] countWordLengths(FileResource resource, int[] counts){
         // Check every word
         for (String word: resource.words()){
             int charCount = 0;
@@ -20,23 +20,37 @@ public class WordLengths {
             // Store in counts array
             counts[charCount] += 1;
         }
-        for (int j=0; j<counts.length; j++){
-            if (counts[j] != 0) {
-                System.out.println(counts[j]+" words of length "+j);
+        return counts;
+    }
+
+
+    public int indexOfMax(int[] values){
+        int maxSoFar = 0;
+        int maxIdx = 0;
+        for (int i=0; i<values.length; i++){
+            if (values[i] > maxSoFar){
+                maxSoFar = values[i];
+                maxIdx = i;
             }
         }
+        return maxIdx;
     }
 
     
     public void testCountWordLengths(){
         FileResource fr = new FileResource();
         int[] counts = new int[31];
-        countWordLengths(fr, counts);
+        int[] mycounts = countWordLengths(fr, counts);
+        int maxInd = indexOfMax(mycounts);
+
+        System.out.println("Most frequent: "+maxInd);
+
+        for (int i=0; i<mycounts.length; i++){
+            if (mycounts[i] != 0) {
+                System.out.println(mycounts[i]+" words of length "+i);
+            }
+        }
     }
-
-
-
-
 
 
     public static void main (String[] args){
