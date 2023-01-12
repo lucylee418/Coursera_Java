c516b3980c62e1e6d59a0a9398d7d2cac7ea0a36
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-12 10:45:53 -0500

    Updated

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -4,25 +4,6 @@ import java.util.Random;
 
 public class ArrayPractice {
 
-    public void simpleSimulate(int rolls){
-        Random rand = new Random();
-        int twos = 0;
-        int twelves = 0;
-
-        for(int i=0; i<rolls; i++){
-            int d1 = rand.nextInt(6) +1;
-            int d2 = rand.nextInt(6) +1;
-            if (d1+d2 == 2){
-                twos+=1;
-            }
-            else if (d1+d2 == 12){
-                twelves+=1;
-            }
-        }
-        System.out.println("2: "+twos+"\n12: "+twelves);
-    }
-
-
     public void simulate(int rolls){
         Random rand = new Random();
         int[] counts = new int[13];
@@ -39,13 +20,70 @@ public class ArrayPractice {
     }
 
 
+    public int indexOf(String[] list, String word){
+        for(int i=0; i<list.length;i++){
+            if (list[i].equals(word)){
+                return i;
+            }
+        }
+        // If the word is not in the list, return -1
+        return -1;
+    }
+
+
+    public void countWords(FileResource resource, String[] common, int[] counts){
+        // Check every word in the file
+        for (String word : resource.words()){
+            word = word.toLowerCase();
+            int index = indexOf(common, word);
+            // If the word exists in comon array,
+            if (index != -1){
+                // increase the corresponding count
+                counts[index] += 1;
+            }
+        }
+    }
+
+
+    public String[] getCommon(){
+        FileResource resource = new FileResource("CommonWordsData/common.txt");
+        // Will get first 20 common words
+        String[] common = new String[20];
+        int index = 0;
+        // Check every word in the file
+        for (String s:resource.words()){
+            common[index] = s;
+            index += 1;
+        }
+        return common;
+    }
+
+
+    public void countShakespeare(){
+        // Array of files we will check
+        String[] plays = {"caesar.txt", "errors.txt", "hamlet.txt", "likeit.txt", "macbeth.txt", "romeo.txt"};
+        // Array of 20 common words
+        String[] common = getCommon();
+        // Initialize
+        int[] counts = new int[common.length];
+        // Check every file
+        for(int i=0; i<plays.length;i++){
+            FileResource resource = new FileResource("CommonWordsData/"+plays[i]);
+            countWords(resource, common, counts);
+            System.out.println("done with "+plays[i]);
+        }
+        for(int i=0; i<common.length; i++){
+            System.out.println(common[i]+"'s count: "+counts[i]);
+        }
+    }
 
 
 
     public static void main(String[] args) {
         ArrayPractice ap = new ArrayPractice();
         // ap.simpleSimulate(10000);
-        ap.simulate(10000);
+        // ap.simulate(10000);
+        ap.countShakespeare();
     }
 
 
