b083973302876bb2005ce8d51f691a6a000fbc03
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-12 10:20:48 -0500

    Created

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -13,19 +13,41 @@ public class ArrayPractice {
             int d1 = rand.nextInt(6) +1;
             int d2 = rand.nextInt(6) +1;
             if (d1+d2 == 2){
-                twos++;
+                twos+=1;
             }
             else if (d1+d2 == 12){
-                twelves++;
+                twelves+=1;
             }
         }
-        System.out.println("2: "+twos+"\n 12:"+twelves);
+        System.out.println("2: "+twos+"\n12: "+twelves);
     }
+
+
+    public void simulate(int rolls){
+        Random rand = new Random();
+        int[] counts = new int[13];
+
+        for(int i=0; i<rolls; i++){
+            int d1 = rand.nextInt(6)+1;
+            int d2 = rand.nextInt(6)+1;
+            int mysum = d1+d2;
+            counts[mysum] += 1;
+        }
+        for(int i=2; i<13; i++){
+            System.out.println(i+": "+counts[i]);
+        }
+    }
+
+
+
+
 
     public static void main(String[] args) {
         ArrayPractice ap = new ArrayPractice();
-        ap.simpleSimulate(7);
+        // ap.simpleSimulate(10000);
+        ap.simulate(10000);
     }
+
 
     
 }
