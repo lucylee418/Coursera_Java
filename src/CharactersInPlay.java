29adf80b7e3258756c3420643c142c72d3dfa7bd
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-14 12:10:06 -0500

    Created

---
+++ src/CharactersInPlay.java
@@ -0,0 +1,17 @@
+import edu.duke.*;
+import java.util.*;
+import java.io.File;
+
+public class CharactersInPlay {
+    private ArrayList<String> myChar;
+    private ArrayList<Integer> myFreqs;
+
+    public CharactersInPlay(){
+        myChar = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
+    }
+
+
+
+    
+}
