d99a474eec3025601c232e422f5af19f0dd669f1
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-14 10:36:58 -0500

    Updated

--- src/WordsWithArrays.java
+++ src/WordsWithArrays.java
@@ -2,7 +2,7 @@ import edu.duke.*;
 import java.util.*;
 
 public class WordsWithArrays {
-    StorageResouce myWords;
+    StorageResource myWords;
 
     public WordsWithArrays(){
         myWords = new StorageResource();
@@ -12,14 +12,47 @@ public class WordsWithArrays {
     public void readWords(){
         myWords.clear();
         FileResource resource = new FileResource();
-        for (Strong word : resource.words()){
+        for (String word : resource.words()){
             myWords.add(word.toLowerCase());
         }
     }
 
 
+    public boolean contains(String[] list, String word, int number){
+        for (int i=0; i<number; i++){
+            if (list[i].equals(word)){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
+    public int numberOfUniqueWords(){
+        int numStored = 0;
+        String[] words = new String[myWords.size()];
+        for (String s : myWords.data()){
+            if (! contains(words, s, numStored)){
+                words[numStored] = s;
+                numStored++;
+            }
+        }
+        return numStored;
+    }
+
+
+    public void tester(){
+        readWords();
+        System.out.println("number of words read: "+ myWords.size());
+        int unique = numberOfUniqueWords();
+        System.out.println("array count "+unique);
+    }
+
+
+    public static void main (String[] args) {
+        WordsWithArrays wwa = new WordsWithArrays();
+        wwa.tester();
+    }
 
 
     
