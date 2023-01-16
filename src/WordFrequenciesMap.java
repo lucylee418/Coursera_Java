3a6781c7b5f113db7149ab294b6fd5a0bd90bd40
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-15 19:46:16 -0500

    Created

---
+++ src/WordFrequenciesMap.java
@@ -0,0 +1,38 @@
+import edu.duke.*;
+import java.util.*;
+
+public class WordFrequenciesMap {
+
+    public void countWords(){
+        FileResource fr = new FileResource();
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        int total = 0;
+        for (String w : fr.words()){
+            w = w.toLowerCase();
+            // If the word w already exists,
+            if (map.keySet().contains(w)){
+                map.put(w, map.get(w)+1);
+            }
+            // Else,
+            else {
+                map.put(w, 1);
+            }
+        }
+        for (String w: map.keySet()){
+            int occurrences = map.get(w);
+            if (occurrences > 500){
+                System.out.println(occurrences+"\t"+w);
+            }
+        }
+        
+    }
+ 
+
+    public static void main (String[] args) {
+        WordFrequenciesMap wfm = new WordFrequenciesMap();
+        wfm.countWords();
+        
+    }
+    
+
+}
